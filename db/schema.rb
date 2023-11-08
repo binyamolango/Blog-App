290359a5e697931daa509ea36383a67e082c208e
binyamolango <olangobinyam@gmail.com>
2023-11-08 17:58:56 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,81 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-def define_schema
-  ActiveRecord::Schema.define(version: 20_231_108_083_850) do
-    enable_extension
-    define_comments_table
-    define_likes_table
-    define_posts_table
-    define_users_table
-    define_foreign_keys
-  end
-end
-
-def enable_extension
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_083850) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
-end
+  enable_extension "plpgsql"
 
-def define_comments_table
-  create_table 'comments', force: :cascade do |t|
-    t.text 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'address'
-    t.bigint 'user_id', null: false
-    t.bigint 'post_id', null: false
-    t.index ['address'], name: 'index_comments_on_address'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["address"], name: "index_comments_on_address"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
-end
 
-def define_likes_table
-  create_table 'likes', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'address'
-    t.bigint 'user_id', null: false
-    t.bigint 'post_id', null: false
-    t.index ['address'], name: 'index_likes_on_address'
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["address"], name: "index_likes_on_address"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
-end
 
-def define_posts_table
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.text 'text'
-    t.bigint 'comments_count'
-    t.bigint 'likes_count'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'address'
-    t.bigint 'author_id', null: false
-    t.index ['address'], name: 'index_posts_on_address'
-    t.index ['author_id'], name: 'index_posts_on_author_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.bigint "comments_count"
+    t.bigint "likes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
+    t.bigint "author_id", null: false
+    t.index ["address"], name: "index_posts_on_address"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
-end
 
-def define_users_table
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'photo'
-    t.text 'bio'
-    t.bigint 'posts_count', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'address'
-    t.index ['address'], name: 'index_users_on_address'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.bigint "posts_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
+    t.index ["address"], name: "index_users_on_address"
   end
-end
 
-def define_foreign_keys
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'posts', 'users', column: 'author_id'
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
