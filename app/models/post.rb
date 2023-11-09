6e859f547060a4e45ac8e1893939e8973b24a748
binyamolango <olangobinyam@gmail.com>
2023-11-09 17:47:33 +0300

    Add recent_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def update_posts_counter
+    author.!increment(:posts_count)
+  end
+
+  def recent_comments
+    comments.order(updated_at: :desc).limit(5)
+  end
 end
