f6aa6231cbb4e63c6348307633f1bd30679d4125
binyamolango <olangobinyam@gmail.com>
2023-11-10 08:30:23 +0300

    Add validation to likes and commens counter in the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   validates :title, presence: true, length: { maximum: 250 }
-  
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_posts_counter
 
