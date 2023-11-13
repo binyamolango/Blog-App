1e02f68875a59e48509375a91cb71840cb1103d1
binyamolango <olangobinyam@gmail.com>
2023-11-13 15:49:05 +0300

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :users, only: [:index, :show]
-  namespace :users do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
