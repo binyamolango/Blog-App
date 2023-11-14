d0b994eb61387e2995f325cc5d06e334d3aeda18
binyamolango <olangobinyam@gmail.com>
2023-11-14 19:42:48 +0300

    Add a test for checking if the reponse body has the correct placeholder

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe "Users", type: :request do
     it "returns http success" do
       get "/users"
       expect(response).to have_http_status(:success) #200
+      expect(response.body).to include("Here is a list of users.")
     end
   end
 
   describe "Get /show" do
     it "return a specific user" do
       user = User.create(name: "Binyam", photo: "url", bio: "I am a software developer.")
-      user_url = "/users"
       get user_url(user)
       expect(response).to be_successful
+      expect(response.body).to include("Here is a specific user.")
     end
   end
 end
