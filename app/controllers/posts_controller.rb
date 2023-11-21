24b9eaac98cf84272067364befda3caea8195b19
binyamolango <olangobinyam@gmail.com>
2023-11-21 22:41:05 +0300

    Update application_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_user
 
   def index
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     @post = Post.new(author_id: @user, title: params[:post][:title], text: params[:post][:text])
     @post.author_id = @user.id
     @post.save
-    redirect_to user_posts_path(@user)
+    redirect_to user_posts_path(@post.author.id, @post.id)
   end
 
   private
