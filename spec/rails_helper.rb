e9139c669c14cb45d8bbac0cc0a325ddb97ed004
binyamolango <olangobinyam@gmail.com>
2023-11-22 16:11:16 +0300

    Update test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Capybara.default_driver = :selenium_chrome
+Capybara.register_driver :chrome_headless do |app| Capybara::Selenium::Driver.new app, browser: :chrome, clear_session_storage: true, clear_local_storage: true, capabilities: [Selenium::WebDriver::Chrome::Options.new( args: %w[headless disable-gpu no-sandbox window-size=1024,768], )] end
