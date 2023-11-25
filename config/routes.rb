267ec3aad62fdee08c5c7f7268558cc1d34acc3d
Binyam Yohannes <125895809+binyamolango@users.noreply.github.com>
2023-11-25 19:35:18 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :confirmations => 'devise/confirmations' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get 'up' => 'rails/health#show', as: :rails_health_check
+  devise_for :users
   devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#destroy'
-    root to: 'devise/sessions#new'
+    authenticated :user do
+      root to: 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
 
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+  resources :users, only: %i[index show] do
+    resources :posts do
+      resources :comments
       resources :likes, only: [:create]
     end
   end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    resources :users, only: [:index] do
+      resources :posts, only: [:index] do
+        resources :comments, only: %i[index create]
+      end
+    end
+  end
 end
