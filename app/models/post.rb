b3ec2997b7a908126b70c1fbf6b3cf2bfc773988
Binyam Yohannes <125895809+binyamolango@users.noreply.github.com>
2023-11-25 20:18:21 +0000

    Update models to fix he API endpoints

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,25 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes
-  has_many :comments
-
-  after_create :update_posts_counter
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def update_posts_counter
-    author.increment!(:posts_count)
+  def recent_five_comments
+    comments.last(5)
   end
 
-  def recent_comments
-    comments.order(updated_at: :desc).limit(5)
+  after_destroy :decrement_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
+
+  def decrement_post_counter
+    author.decrement!(:post_counter)
   end
 end
