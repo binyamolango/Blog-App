a3138f0d07da40392d7f734ea88dacb524875a62
binyamyohannes <olangobinyam@gmail.com>
2023-11-29 19:14:57 +0300

    Add comment delete functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,17 +3,22 @@ class Ability
 
   def initialize(user)
     can :read, Post
+    can :read, Comment
+    can :create, Comment
+    can :update, Comment, user: user
 
     if user.is?("default")  # additional permissions for logged in users (they can read their own posts)
-      can :read, Post, author: user
       can :create, Post, author: user
       can :destroy, Post, author: user
       can :update, Post, author: user
+
+      can :destroy, Comment, user: user
     else
-      can :read, Post
       can :create, Post
       can :destroy, Post
       can :update, Post
+
+      can :destroy, Comment
     end
   end
 end
