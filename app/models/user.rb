c927989f055fbb8cd826966d7fe7b8c157ab0871
binyamyohannes <olangobinyam@gmail.com>
2023-11-29 22:41:21 +0300

    Update application.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  attr_accessor :role
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
