1304651806077727c0acf5628ef07881b969f88b
binyamyohannes <olangobinyam@gmail.com>
2023-11-30 21:22:10 +0300

    Fix linter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,14 @@ class Ability
     can :read, Post
     can :read, Comment
     can :create, Comment
-    can :update, Comment, user: user
-  
-    if user.is?("default")  # additional permissions for logged in users (they can read their own posts)
+    can(:update, Comment, user:)
+
+    if user.is?('default') # additional permissions for logged in users (they can read their own posts)
       can :create, Post, author: user
       can :destroy, Post, author: user
       can :update, Post, author: user
 
-      can :destroy, Comment, user: user
+      can :destroy, Comment, user:
     else
       can :create, Post
       can :destroy, Post
